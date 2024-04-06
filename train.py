9e3676709ee5eb21d80154aaab805f7a9f70e94d
renugadevik <renugadevik@iisc.ac.in>
2024-04-06 23:14:19 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,9 @@
+
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 import pickle
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).to_numpy()
@@ -9,7 +11,16 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-model = LogisticRegression().fit(X, y)
+#model = LogisticRegression().fit(X, y)
 
-with open("model.pkl", 'wb') as f:
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+model = LogisticRegression(max_iter=1000).fit(X_scaled, y)
+
+#model = LogisticRegression(max_iter=1000)  # Increase from default (100)
+#model.fit(X, y)
+#model.fit(X, y)
+
+with open("/app/data/model.pkl", 'wb') as f:
     pickle.dump(model, f)
+~                                 
