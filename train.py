84334c1562fe4f629ea1f4a4e89e301526cb9e78
renugadevik <renugadevik@iisc.ac.in>
2024-04-08 04:40:55 +0530

    branch Initial version

--- train.py
+++ train.py
@@ -1,41 +1,29 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier, GradientBoostingClassifier
 import pickle
-from sklearn.model_selection import GridSearchCV
-from sklearn.preprocessing import LabelEncoder
+import numpy as np
 
-# Load the data
 df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).to_numpy()
 y = df['Disease'].to_numpy()
-le = LabelEncoder()
-y = le.fit_transform(y)
+labels = np.sort(np.unique(y))
+y = np.array([np.where(labels == x) for x in y]).flatten()
 
-# Define the parameter grid
-param_grid = {
-    'n_estimators': [3000, 5000],
-    'max_depth': [30, 40, 50],
-    'min_samples_split': [2, 5, 10],
-    'min_samples_leaf': [1, 2, 4],
-    'max_features': ['auto', 'sqrt', 'log2']
-}
+#rf = RandomForestClassifier(n_estimators=100, max_depth=15, min_samples_split=5, min_samples_leaf=2)  # Increase the number of estimators and tune max_depth
+#gb = GradientBoostingClassifier(n_estimators=100, max_depth=5, learning_rate=0.1)  # Increase the number of estimators and tune max_depth
 
-# Initialize the RandomForestClassifier
-rf = RandomForestClassifier(random_state=42)
+#rf = RandomForestClassifier(n_estimators=300, max_depth=20, min_samples_split=10, min_samples_leaf=2, random_state=42)
+#gb = GradientBoostingClassifier(n_estimators=300, max_depth=5, learning_rate=0.1, random_state=42)
+rf = RandomForestClassifier()
+gb = GradientBoostingClassifier()
 
-# Initialize GridSearchCV
-grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=3, scoring='accuracy', n_jobs=-1)
+# Adding a VotingClassifier classifier with multiple classifiers to boost the score.
+model = VotingClassifier(
+    estimators=[
+        ('rf', rf),
+        ('gb', gb)
+    ],
+).fit(X, y)
 
-# Perform GridSearchCV
-grid_search.fit(X, y)
-
-# Get the best estimator
-best_rf = grid_search.best_estimator_
-
-# Train the model using the best estimator
-model = best_rf.fit(X, y)
-
-# Save the model
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
-print("End train")
