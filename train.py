deb1820266480c56398f275fc60e19f8320825d0
renugadevik <renugadevik@iisc.ac.in>
2024-04-07 19:46:32 +0530

    change in train file in new nranch

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
 import pickle
 import numpy as np
 
@@ -9,7 +10,24 @@ y = df['Disease'].to_numpy()
 labels = np.sort(np.unique(y))
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-model = LogisticRegression().fit(X, y)
+# Define the hyperparameters to tune
+param_grid = {
+    'n_estimators': [50, 100, 200],
+    'max_depth': [None, 10, 20, 30],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+    'bootstrap': [True, False]
+}
+
+# Create a GridSearchCV object
+grid_search = GridSearchCV(RandomForestClassifier(), param_grid, cv=5, n_jobs=-1)
+
+# Fit the model
+grid_search.fit(X, y)
+
+# Get the best model
+best_model = grid_search.best_estimator_
 
 with open("model.pkl", 'wb') as f:
-    pickle.dump(model, f)
+    pickle.dump(best_model, f)
+
