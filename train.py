bb82e7ccf5a4ab18956bd65aaef932321b04fddd
renugadevik <renugadevik@iisc.ac.in>
2024-04-07 20:03:22 +0530

    Change in train and compose file

--- train.py
+++ train.py
@@ -1,32 +1,27 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import randint
 import pickle
 import numpy as np
 
-df = pd.read_csv("data/train.csv")
-X = df.drop(columns=['Disease']).to_numpy()
-y = df['Disease'].to_numpy()
-labels = np.sort(np.unique(y))
-y = np.array([np.where(labels == x) for x in y]).flatten()
-
-# Define the hyperparameters to tune
-param_grid = {
-    'n_estimators': [50, 100, 200],
+# Define the hyperparameters to search
+param_dist = {
+    'n_estimators': randint(50, 500),
     'max_depth': [None, 10, 20, 30],
-    'min_samples_split': [2, 5, 10],
-    'min_samples_leaf': [1, 2, 4],
+    'min_samples_split': randint(2, 20),
+    'min_samples_leaf': randint(1, 10),
     'bootstrap': [True, False]
 }
 
-# Create a GridSearchCV object
-grid_search = GridSearchCV(RandomForestClassifier(), param_grid, cv=5, n_jobs=-1)
+# Create a RandomizedSearchCV object
+random_search = RandomizedSearchCV(RandomForestClassifier(), param_distributions=param_dist, n_iter=20, cv=5, n_jobs=-1)
 
 # Fit the model
-grid_search.fit(X, y)
+random_search.fit(X, y)
 
 # Get the best model
-best_model = grid_search.best_estimator_
+best_model = random_search.best_estimator_
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(best_model, f)
